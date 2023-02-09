0bc76e663e1fb6203da46ecc41a4525eb86db8ab
Drew Immerman <drewimmerman@gmail.com>
2023-02-09 07:08:14 -0500

    Add DatabaseDriver::queryBuilder so user's can get the appropriate builder for a connection

--- src/driver.ts
+++ src/driver.ts
@@ -1,8 +1,10 @@
 import { DatabaseConnectionOptions } from './connection-options';
 import { DatabaseQueryResult } from './query';
+import { DatabaseQueryBuilder } from './query-builder';
 
 export interface DatabaseDriver {
 	connect: (options: DatabaseConnectionOptions) => Promise<void>;
 	disconnect: () => Promise<void>;
 	query: (QueryOptions) => Promise<DatabaseQueryResult>;
+	queryBuilder: typeof DatabaseQueryBuilder;
 }
