403141ac4eb7ac149eb877eb11e3f0a148e46f33
Drew Immerman <drewimmerman@gmail.com>
2023-02-12 12:29:48 -0500

    Add data definition builder to driver

--- src/driver.ts
+++ src/driver.ts
@@ -1,23 +1,45 @@
 import { DatabaseConnectionOptions } from './connection-options';
-import { DatabaseQueryOptions, DatabaseQueryResult } from './query';
+import {
+	DatabaseQueryOptions,
+	DatabaseQueryResult,
+	DatabaseQueryTypes,
+} from './query';
 import { DatabaseQueryBuilder } from './dml/query-builder';
+import { DataDefinitionBuilder } from './ddl/ddl-builder';
+import { Builder } from './builder';
 
 export interface DatabaseDriverInterface {
+	dataDefinitionBuilder: typeof DataDefinitionBuilder;
+	queryBuilder: typeof DatabaseQueryBuilder;
+
 	connect: (options: DatabaseConnectionOptions) => Promise<this>;
 	disconnect: () => Promise<void>;
-	query: (
-		options: DatabaseQueryOptions | DatabaseQueryBuilder
-	) => Promise<DatabaseQueryResult>;
-	queryBuilder: typeof DatabaseQueryBuilder;
+	query: (options: DatabaseQueryTypes) => Promise<DatabaseQueryResult>;
+	getDataDefinitionBuilder: () => DataDefinitionBuilder;
 	getQueryBuilder: () => DatabaseQueryBuilder;
 	getVersion: () => Promise<string>;
 }
 
 export abstract class BaseDatabaseDriver {
+	public dataDefinitionBuilder: typeof DataDefinitionBuilder;
 	public queryBuilder: typeof DatabaseQueryBuilder;
+
+	public getDataDefinitionBuilder(): DataDefinitionBuilder {
+		return new this.dataDefinitionBuilder();
+	}
 
 	public getQueryBuilder(): DatabaseQueryBuilder {
 		return new this.queryBuilder();
+	}
+
+	protected toDatabaseQueryOptions(
+		from: DatabaseQueryTypes
+	): DatabaseQueryOptions {
+		if (from instanceof Builder) {
+			return from.toDatabaseQuery();
+		}
+
+		return from;
 	}
 }
 
--- src/query.ts
+++ src/query.ts
@@ -1,7 +1,15 @@
+import { DataDefinitionBuilder } from './ddl';
+import { DatabaseQueryBuilder } from './dml';
+
 export interface DatabaseQueryOptions {
 	sql: string;
 	params?: any[];
 }
+
+export type DatabaseQueryTypes =
+	| DatabaseQueryOptions
+	| DataDefinitionBuilder
+	| DatabaseQueryBuilder;
 
 export interface DatabaseQueryResult {
 	results?: Record<string, any>[];
