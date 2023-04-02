734974cb6236333592cceb6b9ae2b24e35fa444d
Drew Immerman <drewimmerman@gmail.com>
2023-04-02 13:44:16 -0400

    Add repository support for alter table

--- src/repository/ddl-repository.ts
+++ src/repository/ddl-repository.ts
@@ -1,3 +1,11 @@
+import {
+	AddColumnsOptions,
+	AddForeignKeyOptions,
+	ChangeColumnOptions,
+	DropForeignKeyOptions,
+	RenameColumnOptions,
+	RenameTableOptions,
+} from '../ddl/alter-table';
 import { CreateTableOptions, DropTableOptions, TruncateOptions } from '../ddl';
 import { Repository } from './repository';
 
@@ -13,6 +21,83 @@ export class DataDefinitionRepository extends Repository {
 	public async createTable(options: CreateTableOptions): Promise<void> {
 		await this.driver.query(
 			this.driver.getDataDefinitionBuilder().createTable(options)
+		);
+	}
+
+	/**
+	 * Add columns to a table
+	 *
+	 * @param options Add column options
+	 */
+	public async addColumns(options: AddColumnsOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().addColumns(options)
+		);
+	}
+
+	/**
+	 * Add a foreign key to a table
+	 *
+	 * @param options Foreign key options
+	 */
+	public async addForeignKey(options: AddForeignKeyOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().addForeignKey(options)
+		);
+	}
+
+	/**
+	 * Change a column
+	 *
+	 * @param options Column options
+	 */
+	public async changeColumn(options: ChangeColumnOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().changeColumn(options)
+		);
+	}
+
+	/**
+	 * Drop a column
+	 *
+	 * @param options Drop options
+	 */
+	public async dropColumn(options: ChangeColumnOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().dropColumn(options)
+		);
+	}
+
+	/**
+	 * Drop a foreign key constraint
+	 *
+	 * @param options Drop options
+	 */
+	public async dropForeignKey(options: DropForeignKeyOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().dropForeignKey(options)
+		);
+	}
+
+	/**
+	 * Rename a column
+	 *
+	 * @param options Rename options
+	 */
+	public async renameColumn(options: RenameColumnOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().renameColumn(options)
+		);
+	}
+
+	/**
+	 * Rename a table
+	 *
+	 * @param options Rename options
+	 */
+	public async renameTable(options: RenameTableOptions): Promise<void> {
+		await this.driver.query(
+			this.driver.getDataDefinitionBuilder().renameTable(options)
 		);
 	}
 
