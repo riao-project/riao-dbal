0190e99b3c9375197ad9ea68ffaec286be043de1
Drew Immerman <drewimmerman@gmail.com>
2023-04-30 08:48:26 -0400

    Fix databases without names

--- test/sample/database/main/index.ts
+++ test/sample/database/main/index.ts
@@ -1,5 +1,7 @@
 import { TestDatabase } from '../../../util/database';
 
-export default class MainDatabase extends TestDatabase {}
+export default class MainDatabase extends TestDatabase {
+	name = 'main';
+}
 
 export const maindb = new MainDatabase();
--- test/util/database.ts
+++ test/util/database.ts
@@ -4,6 +4,7 @@ import { TestDatabaseDriver } from './driver';
 export class TestDatabaseEnv extends DatabaseEnv {}
 
 export class TestDatabase extends Database {
+	name = 'test';
 	driverType = TestDatabaseDriver;
 	envType = TestDatabaseEnv;
 }
