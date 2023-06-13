53596da427bf2f2ad540ad2668cf3ad8244c6228
Drew Immerman <drewimmerman@gmail.com>
2023-06-13 18:51:17 -0400

    Remove loadAll() function

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,7 @@
 export { configureDb } from './configure-db';
-export { DatabaseConnectionOptions } from './connection-options'
+export { DatabaseConnectionOptions } from './connection-options';
 export { DatabaseEnv } from './database-env';
 export { Database } from './database';
 export { DatabaseDriver } from './driver';
 export { getDatabasePath } from './get-database-path';
-export { DatabaseByName, loadAll } from './load-all';
 export { loadDatabase } from './load-database';
--- test/spec/database/load-all.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'jasmine';
-import { Database, loadAll } from '../../../src/';
-
-describe('database/loadAll()', () => {
-	it('can load all databases', async () => {
-		const databases = await loadAll('test/sample/database');
-		expect(databases.main).toBeInstanceOf(Database);
-	});
-});
